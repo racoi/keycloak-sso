0385deac20140ccc0ff6e2e9f91d410d9b04cb0b
racoi <919jung@gmail.com>
2025-11-18 22:01:31 +0900

    init

---
+++ src/main/java/com/example/keycloak_sso/KeycloakSsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloak_sso/KeycloakSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
